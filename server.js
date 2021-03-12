f6594095ad245c2969fea512477e53bd19f219ae
Timilehin Aderinola <timiderinola@gmail.com>
2021-03-12 17:30:03 +0800

    Init server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
